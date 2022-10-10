dbb670001c2dc1d7a0c8449f8fc57688db231dc9
63070078 <63070078@kmitl.ac.th>
2022-10-10 20:49:27 +0700

    Initial commit

---
+++ src/test/java/com/sop/chapter8/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.chapter8.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
